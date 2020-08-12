e0851c455201d4e0622461e716fcecdc8823ec02
Erik <robere2@rpi.edu>
2020-08-11 21:03:53 -0400

    Initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,59 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <VApp dark>
+    <VAppBar app color="teal darken-1" dark>
+      <div class="d-flex align-center">
+        <VImg
+          alt="Quickplay Logo"
+          class="shrink mr-2"
+          contain
+          src="/img/quickplay-white.png"
+          transition="scale-transition"
+          width="40"
+        />
+
+        <h1>Quickplay</h1>
+      </div>
+
+      <div class="nav-buttons">
+        <VBtn text>
+          Home
+        </VBtn>
+      </div>
+    </VAppBar>
+
+    <VMain class="main-body">
+      <QuickplayTitle />
+    </VMain>
+  </VApp>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
+<script lang="ts">
+import Vue from "vue";
+import QuickplayTitle from "@/components/QuickplayTitle.vue";
 
-export default {
-  name: 'App',
+export default Vue.extend({
+  name: "App",
+
   components: {
-    HelloWorld
-  }
-}
+    QuickplayTitle
+  },
+
+  data: () => ({
+    //
+  })
+});
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style lang="scss" scoped>
+@import "~vuetify/src/styles/styles.sass";
+.nav-buttons {
+  margin-left: 20px;
+  height: 100%;
+  button {
+    height: 100% !important;
+  }
+}
+.main-body {
+  background-color: map-get($grey, darken-4);
 }
 </style>
---
+++ src/views/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="home">
+    <img alt="Logo" src="../assets/quickplay-white.png" />
+    <QuickplayTitle />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import QuickplayTitle from "@/components/QuickplayTitle.vue";
+
+export default {
+  name: "index",
+  components: {
+    QuickplayTitle
+  }
+};
+</script>
